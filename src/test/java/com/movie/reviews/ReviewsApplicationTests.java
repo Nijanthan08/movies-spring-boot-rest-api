a32930f7583f335410a5bb32ff7df88f375f5216
Nijanthan <nijanth.niji@gmail.com>
2019-10-05 20:44:28 +0530

    Initialized REST API

---
+++ src/test/java/com/movie/reviews/ReviewsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.movie.reviews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReviewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
