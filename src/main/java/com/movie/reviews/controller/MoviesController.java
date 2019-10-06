ae7fe134e9cfc8705a48e834ba3354fde5ba2d91
Nijanthan <nijanth.niji@gmail.com>
2019-10-06 14:14:52 +0530

    Add request mapping to fetch all Movies

--- src/main/java/com/movie/reviews/controller/MoviesController.java
+++ src/main/java/com/movie/reviews/controller/MoviesController.java
@@ -1,8 +1,12 @@
 package com.movie.reviews.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +16,12 @@ import com.movie.reviews.domain.Languages;
 import com.movie.reviews.domain.Movies;
 import com.movie.reviews.service.MoviesService;
 
+
 @RestController
 @RequestMapping("/movies")
 public class MoviesController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(MoviesController.class);
 
 	@Autowired
 	private MoviesService moviesService;
@@ -28,9 +35,26 @@ public class MoviesController {
 	public List<Genres> getAllGenres() {
 		return moviesService.getAllGenres();
 	}
-	
+
+	@RequestMapping(value = "/popular", method = RequestMethod.GET)
+	public List<Movies> getPopularMovies() {
+		LOG.info("Fetch Popular Movies...");
+		List<Movies> list = moviesService.getAllMovies();
+		LOG.debug(": " + list.size());
+		List<Movies> ratedMovies = list.parallelStream().filter(obj -> null != obj.getRating())
+				.collect(Collectors.toList());
+		LOG.debug(": " + ratedMovies.size());
+		return ratedMovies;
+	}
+
+	@RequestMapping(value = "/{movieId}", method = RequestMethod.GET)
+	public List<Movies> getMovieInfo(@PathVariable("movieId") Integer movieId) {
+		return moviesService.getMovieInfo(movieId);
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Movies> getAllMovies() {
+		LOG.info("Fetch all the Movies...");
 		return moviesService.getAllMovies();
 	}
 
--- src/main/java/com/movie/reviews/service/MoviesService.java
+++ src/main/java/com/movie/reviews/service/MoviesService.java
@@ -1,8 +1,10 @@
 package com.movie.reviews.service;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,9 +12,11 @@ import org.springframework.stereotype.Service;
 import com.movie.reviews.domain.Genres;
 import com.movie.reviews.domain.Languages;
 import com.movie.reviews.domain.Movies;
+import com.movie.reviews.domain.Reviews;
 import com.movie.reviews.repository.GenreRepository;
 import com.movie.reviews.repository.LanguageRepository;
 import com.movie.reviews.repository.MoviesRepository;
+import com.movie.reviews.repository.ReviewRepository;
 
 @Service
 public class MoviesService {
@@ -26,6 +30,9 @@ public class MoviesService {
 	@Autowired
 	private MoviesRepository moviesRepository;
 
+	@Autowired
+	private ReviewRepository reviewRepository;
+
 	public List<Languages> getAllLanguages() {
 		return (List<Languages>) languageRepository.findAll();
 	}
@@ -35,6 +42,27 @@ public class MoviesService {
 	}
 
 	public List<Movies> getAllMovies() {
+		List<Movies> list = (List<Movies>) moviesRepository.findAll();
+		updateLanguageGenres(list);
+		return list;
+	}
+
+	public List<Movies> getMovieInfo(Integer movieId) {
+
+		Optional<Movies> movieOptional = moviesRepository.findById(movieId);
+		Movies movie = movieOptional.get();
+		//Update Reviews
+		List<Reviews> reviews = reviewRepository.fetchReviewByMovieId(movie.getId());
+		movie.setReviews(reviews);
+		
+		//Update Genre & Language
+		List<Movies> list = Arrays.asList(new Movies[] { movie });
+		updateLanguageGenres(list);
+
+		return list;
+	}
+
+	private void updateLanguageGenres(List<Movies> list) {
 		Map<Integer, String> languageMap = new HashMap<>();
 		Map<Integer, String> genreMap = new HashMap<>();
 
@@ -44,13 +72,11 @@ public class MoviesService {
 		List<Genres> genreList = getAllGenres();
 		genreList.parallelStream().forEach(obj -> genreMap.put(obj.getId(), obj.getName()));
 
-		List<Movies> list = (List<Movies>) moviesRepository.findAll();
 		list.parallelStream().forEach(obj -> {
 			obj.setLanguage(languageMap.get(obj.getLanguageId()));
 			obj.setGenre(genreMap.get(obj.getGenreId()));
 		});
 
-		return list;
 	}
 
 }
