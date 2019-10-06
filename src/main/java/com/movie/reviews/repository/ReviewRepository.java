f69d43a6a9ca63c9a972de7607608e927599d7d9
Nijanthan <nijanth.niji@gmail.com>
2019-10-06 14:14:32 +0530

    Added Review Repository

---
+++ src/main/java/com/movie/reviews/repository/ReviewRepository.java
@@ -0,0 +1,16 @@
+package com.movie.reviews.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.movie.reviews.domain.Reviews;
+
+public interface ReviewRepository extends CrudRepository<Reviews, Integer> {
+	
+	@Query("Select reviews from Reviews reviews where reviews.movie= :movieId")
+	List<Reviews> fetchReviewByMovieId(@Param("movieId") Integer movieId);
+
+}
