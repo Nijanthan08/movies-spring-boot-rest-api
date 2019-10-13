23bfde5d4c56a2ae484809f9375181f3da895eb8
Nijanthan <nijanth.niji@gmail.com>
2019-10-13 13:53:33 +0530

    Removed security config

--- src/main/java/com/movie/reviews/AppConfig.java
+++ src/main/java/com/movie/reviews/AppConfig.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
-@EnableWebSecurity
 public class AppConfig {
 
 	public static void main(String[] args) {
