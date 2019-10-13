213d9fb381e0e1c3f1c9e0c27eda4476734a6ec7
Nijanthan <nijanth.niji@gmail.com>
2019-10-13 13:46:54 +0530

    Removed Custom Token Authentication

--- src/main/java/com/movie/reviews/security/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.movie.reviews.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
