f311c4dabbd378679160b9c3241db891c427a17d
Nijanthan <nijanth.niji@gmail.com>
2019-10-19 12:03:47 +0530

    Updated TokenAuthenticationService

--- src/main/java/com/movie/reviews/security/TokenAuthenticationService.java
+++ src/main/java/com/movie/reviews/security/TokenAuthenticationService.java
@@ -10,12 +10,12 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
-import com.movie.reviews.domain.User;
+import com.movie.reviews.domain.Role;
 
 @Service
-public class ManageJsonWebTokens {
+public class TokenAuthenticationService {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ManageJsonWebTokens.class);
+	private static final Logger LOG = LoggerFactory.getLogger(TokenAuthenticationService.class);
 
 	@Autowired
 	private Environment env;
@@ -27,7 +27,7 @@ public class ManageJsonWebTokens {
 			Algorithm algorithm = Algorithm.HMAC256(env.getProperty("privateKey"));
 			token = JWT.create().withClaim("id", user.getId()).withClaim("firstName", user.getFirstName())
 					.withClaim("lastName", user.getLastName()).withClaim("emailId", user.getEmailId())
-					.withClaim("admin", user.getAdmin()).sign(algorithm);
+					.withClaim("admin", user.getAdmin()).withClaim("active", user.getActive()).sign(algorithm);
 		} catch (Exception e) {
 			LOG.error("Exception: " + e);
 		}
@@ -43,9 +43,12 @@ public class ManageJsonWebTokens {
 			JWTVerifier verifier = JWT.require(algorithm).build();
 			DecodedJWT jwtDecoded = verifier.verify(jwt);
 
+			String admin = jwtDecoded.getClaim("admin").asString();
+
 			user = new User(jwtDecoded.getClaim("id").asInt(), jwtDecoded.getClaim("firstName").asString(),
-					jwtDecoded.getClaim("lastName").asString(), jwtDecoded.getClaim("emailId").asString(),
-					jwtDecoded.getClaim("admin").asString());
+					jwtDecoded.getClaim("lastName").asString(), jwtDecoded.getClaim("emailId").asString(), admin,
+					jwtDecoded.getClaim("active").asString(),
+					"Y".equalsIgnoreCase(admin) ? Role.ADMIN.toString() : Role.USER.toString());
 		} catch (Exception exception) {
 			LOG.error("Exception: " + exception);
 		}
